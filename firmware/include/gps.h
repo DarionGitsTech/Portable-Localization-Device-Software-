b64b3b46aab624106960f776dd9ba7153011faa6
DarionGitsTech <darion.taylor@okstate.edu>
2026-04-03 09:31:19 -0500

    added gps.h file

--- firmware/include/gps.h
+++ firmware/include/gps.h
@@ -1,1 +1,18 @@
+#ifndef GPS_H
+#define GPS_H
 
+#include <stdbool.h>
+
+typedef struct {
+    float latitude;
+    float longitude;
+    float altitude;
+    int satellites;
+    bool fix;
+} GPSData;
+
+void gps_init(void);
+bool gps_update(GPSData *data);
+void gps_print(const GPSData *data);
+
+#endif
