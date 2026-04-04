28e257850f10609c29af3ecefc747dd15da654f1
Garrett Fundis <123599265+Durpylord86@users.noreply.github.com>
2026-04-04 15:07:47 -0500

    Add LCD initialization and update functions
    Placeholder since I don't think this will work once we start testing.

--- firmware/src/lcd.cpp
+++ firmware/src/lcd.cpp
@@ -1,1 +1,27 @@
+#include "lcd.h"
+#include <wire.h>
+#include <LiquidCrystal_I2C.h>
 
+// Common I2C address is 0x27 or 0x3F
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void lcd_init() {
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("GPS Initializing");
+}
+
+void lcd_update(GPSData* data) {
+  lcd.clear();
+
+  if(data->valid) {
+    lcd.setCursor(0, 0);
+    lcd.print("Lat:");
+    lcd.print(data->longitude, 2);
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print("No GPS Fix");
+  }
+}
