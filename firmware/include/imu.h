4282af9c8f6ef929f901af7258d5e6e7aa4e7792
Garrett Fundis <123599265+Durpylord86@users.noreply.github.com>
2026-04-06 09:36:18 -0500

    Add IMU header file with data structure and functions
    This is a rough estimate place holder until testing.

--- firmware/include/imu.h
+++ firmware/include/imu.h
@@ -1,1 +1,16 @@
+#ifndef IMU_H
+#define IMU_H
 
+#include <stdbool.h>
+
+typedef struct {
+    float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+} IMUData;
+
+void imu_init(void);
+bool imu_update(IMUData *data);
+void imu_print(const IMUData *data);
+
+#endif
