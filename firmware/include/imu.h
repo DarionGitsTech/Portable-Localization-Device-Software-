8b2a892a1ec04bf1b5607ee20a9593a8479651fd
Garrett Fundis <123599265+Durpylord86@users.noreply.github.com>
2026-04-06 09:54:25 -0500

    Reformat IMUData struct for consistency

--- firmware/include/imu.h
+++ firmware/include/imu.h
@@ -4,19 +4,19 @@
 #include <stdbool.h>
 
 typedef struct {
-float accel_x;
-float accel_y;
-float accel_z;
-
-float gyro_x;
-float gyro_y;
-float gyro_z;
-
-float mag_x;
-float mag_y;
-float mag_z;
-
-bool valid;
+  float accel_x;
+  float accel_y;
+  float accel_z;
+  
+  float gyro_x;
+  float gyro_y;
+  float gyro_z;
+  
+  float mag_x;
+  float mag_y;
+  float mag_z;
+  
+  bool valid;
 } IMUData;
 
 // Initialize IMU
