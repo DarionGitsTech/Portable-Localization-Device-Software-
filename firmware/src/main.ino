2055666dea1805b95a7b0a271a925599b296ae40
DarionGitsTech <darion.taylor@okstate.edu>
2026-04-10 09:37:31 -0500

    tweaked and fixed main.ino

--- firmware/src/main.ino
+++ firmware/src/main.ino
@@ -8,62 +8,51 @@
 GPSData gpsData;
 IMUData imuData;
 
-char csvHeader = [CSV_BUFFER_SIZE];
-char csvRow = [CSV_BUFFER_SIZE];
-
+char csvHeader[CSV_BUFFER_SIZE];
+char csvRow[CSV_BUFFER_SIZE];
 
 void setup() {
     Serial.begin(115200);
-    delay(1000);
+    while (!Serial) {
+        ;
+    }
 
+    delay(500);
     Serial.println("Initializing system...");
 
     gps_init();
     imu_init();
-    ble_init(9600); // HM-10 usually defaults to 9600 baud
+    ble_init(9600);
     lcd_init();
 
-    if(csvlogger_get_header(csvHeader, sizeof(csvHeader)))
-    {
+    if (csvlogger_get_header(csvHeader, sizeof(csvHeader))) {
         Serial.println(csvHeader);
         ble_send_line(csvHeader);
+    } else {
+        Serial.println("Failed to create CSV header.");
     }
-
-    else 
-    {
-        Serial.println("Failed to create the CSV header.");
-    }
-} // end void setup()
-
-
+}
 
 void loop() {
-    bool gps_ok = gps_update(&gpsData)
-    bool imu_ok = imu_update(&imuData)
+    bool gps_ok = gps_update(&gpsData);
+    bool imu_ok = imu_update(&imuData);
 
-    if (gps_ok) 
-    {
+    if (gps_ok) {
         lcd_update(&gpsData);
+    } else {
+        lcd_show_no_fix();
     }
 
-    if(gps_ok && imu_ok)
-    {
-        if(csvlogger_format_row(csvRow, sizeof(csvRow), millis(), &gpsData, &imudata))
-         {
-            Serialprintln(csvRow);
+    if (gps_ok && imu_ok) {
+        if (csvlogger_format_row(csvRow, sizeof(csvRow), &gpsData, &imuData)) {
+            Serial.println(csvRow);
             ble_send_line(csvRow);
-         }
-
-         else
-         {
-            Serialprintln("Failed to print CSV Row.");
-         }
-    }
-    else 
-    {
-        Serialprintln("Senor update failed.");
+        } else {
+            Serial.println("Failed to format CSV row.");
+        }
+    } else {
+        Serial.println("Sensor update failed.");
     }
 
-    
     delay(1000);
-} // end void loop()
+}
